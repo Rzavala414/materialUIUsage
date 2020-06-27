0c45463db6bd1b25ba7156e3864f0625708e3235
Roger Zavala <rogeliozavala15@gmail.com>
2020-06-27 10:50:45 -0700

    created simple menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import SimpleMenu from './Components/SimpleMenu.js'
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <SimpleMenu/>
+    </>
   );
 }
 
