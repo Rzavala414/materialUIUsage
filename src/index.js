51469b0895b14dd3882769bdc8d4bac1a4b63dec
Roger Zavala <rogeliozavala15@gmail.com>
2020-06-27 11:46:59 -0700

    added material ui template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SignUp from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SignUp />
   </React.StrictMode>,
   document.getElementById('root')
 );
